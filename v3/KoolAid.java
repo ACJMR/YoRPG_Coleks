808303904ebcc25888d30479acd1e7f822cbcfa7
jwong25Ulti25 <jwong25@stuy.edu>
2017-11-13 20:58:26 -0500

    Added KoolAid.java and Slime.java as subclasses of superclass Monster.java

---
+++ v3/KoolAid.java
@@ -0,0 +1,20 @@
+/********************
+Team Coleks - Colin Hosking, Aleksandra Koroza, Johnny Wong
+APCS1 pd08
+HW 31 -- Ye Olde Role Playing Game, Expanded
+2017-11-14
+********************/
+
+public class KoolAid extends Monster{
+
+    //Constructor
+    public KoolAid(String name){
+	super(name);
+	defense = 150;
+	currentDef = defense; //Changes defense to 150 instead of default 40
+    }
+
+    public static String about(){
+	return "Flowing from the Rivers of Nebraskaid, KoolAids possess a defense change from 40 --> 150.";
+    }
+}//ends KoolAid
